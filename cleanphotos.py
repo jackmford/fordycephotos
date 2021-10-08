903001cf9e885b0e2585bb720e7b4e473f2c6123
jackmford <jackmitchellfordyce.com>
2021-10-08 21:48:51 +0000

    logic to pull images not in img directory from s3

--- cleanphotos.py
+++ cleanphotos.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+import boto3
+import os
+from flask import render_template
+AWS_KEY=os.environ['AWS_KEY']
+AWS_SECRET=os.environ['AWS_SECRET']
+BUCKET_NAME=os.environ['BUCKET_NAME']
+
+
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    client = boto3.client('s3', aws_access_key_id=AWS_KEY, aws_secret_access_key=AWS_SECRET)
+    contents = client.list_objects(Bucket=BUCKET_NAME)
+    img_files = os.listdir('static/img')
+    for i in range(len(contents['Contents'])):
+        img_name = contents['Contents'][i]['Key']
+        if img_name not in img_files:
+            client.download_file(BUCKET_NAME, img_name, f'static/img/{img_name}')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
