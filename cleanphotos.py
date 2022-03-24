08160c075bcc5f7c3c6bc124aaecbc80fd6e1aae
jackmford <jackmitchellfordyce@gmail.com>
2022-03-24 15:26:52 -0500

    working on some new stuff

--- cleanphotos.py
+++ cleanphotos.py
@@ -13,11 +13,14 @@ BUCKET_NAME=os.environ['BUCKET_NAME']
 def hello():
     client = boto3.client('s3', aws_access_key_id=AWS_KEY, aws_secret_access_key=AWS_SECRET)
     contents = client.list_objects(Bucket=BUCKET_NAME)
-    img_files = os.listdir('static/img')
+    iphone_img_files = os.listdir('static/img/iphone')
+    film_img_files = os.listdir('static/img/film')
     for i in range(len(contents['Contents'])):
         img_name = contents['Contents'][i]['Key']
-        if img_name not in img_files:
-            client.download_file(BUCKET_NAME, img_name, f'static/img/{img_name}')
+        if img_name not in img_files and 'film' in img_name:
+            client.download_file(BUCKET_NAME, img_name, f'static/img/film/{img_name}')
+        elif img_name not in img_files:
+            client.download_file(BUCKET_NAME, img_name, f'static/img/iphone/{img_name}')
     return render_template('index.html', images=img_files)
 
 if __name__ == "__main__":
