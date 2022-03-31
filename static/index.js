bafeb0bb02ca41068737bf3ddc8f197e71fb9dff
jackmford <jackmitchellfordyce@gmail.com>
2022-03-31 14:57:32 -0500

    messing with loaders idk

--- static/index.js
+++ static/index.js
@@ -15,3 +15,17 @@ function changeTab(film_type, btn_name) {
 
 document.getElementById('film_button').className = "active";
 document.getElementById('iphone').style.display = "none";
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+const loader = document.querySelector(".loader");
+window.onload = function(){
+  setTimeout(function(){
+      loader.style.opacity = "0";
+      setTimeout(function(){
+          loader.style.display = "none";
+          }, 500);
+      },1500);
+}
