7916373760ae3aaecbd7a29bbe8d059141fb5c81
jackmford <jackmitchellfordyce@gmail.com>
2022-05-05 09:24:07 -0500

    Flake8 on cleanphotos.py
    - restructuring to align with pep8 standards

--- cleanphotos.py
+++ cleanphotos.py
@@ -1,9 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-
+from flask import render_template
 import glob
 import os
-from flask import render_template
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -12,7 +13,9 @@ def hello():
 
     iphone_img_files = glob.glob('static/img/iphone/*')
     iphone_img_files.sort(key=os.path.getmtime)
-    return render_template('index.html', iphone_images=iphone_img_files, film_images=film_img_files)
+    return render_template('index.html', iphone_images=iphone_img_files,
+                           film_images=film_img_files)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
