4c55e6dcb36ac1ceefa4c0f5348b8b48e1a16614
jackmford <jackmitchellfordyce@gmail.com>
2022-05-09 10:23:42 -0500

    Added health endpoint
    - wrote test for health endpoint
    - returns 200 if app launched

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
 from app import cleanphotos
+from flask import Flask
+from http import HTTPStatus
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(cleanphotos.bp)
+
+    @app.route('/health', methods=['GET'])
+    def health():
+        """ Just a health check endpoint. """
+        return 'Healthy', HTTPStatus.OK
 
     return app
--- tests/test_file.py
+++ tests/test_file.py
@@ -1,9 +1,17 @@
 from app import create_app
 
 
-def test_health_endpoint():
+def test_homepage_endpoint():
     app = create_app()
 
     with app.test_client() as test_client:
         response = test_client.get('/')
+        assert response.status_code == 200
+
+
+def test_health_endpoint():
+    app = create_app()
+
+    with app.test_client() as test_client:
+        response = test_client.get('/health')
         assert response.status_code == 200
